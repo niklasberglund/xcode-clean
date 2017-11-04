953d9f5493ce476bbe895ed273d1198b26f9ee56
Niklas Berglund <niklas@asdasd.net>
2017-11-04 18:40:36 +0800

    Created empty project

---
+++ xcode-clean/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xcode-clean
+//
+//  Created by Niklas Berglund on 2017-11-04.
+//  Copyright © 2017 Niklas Berglund. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
