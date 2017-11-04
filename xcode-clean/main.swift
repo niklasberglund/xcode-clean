b4f8bd55ede95c6a2a6184f96eda0ea46f46e586
Niklas Berglund <niklas@asdasd.net>
2017-11-04 19:26:45 +0800

    Implemented -h flag

--- xcode-clean/main.swift
+++ xcode-clean/main.swift
@@ -10,18 +10,24 @@ import Foundation
 
 let commandLine = CommandLine()
 
+let helpFlag = BoolOption(shortFlag: "h", longFlag: "help", required: false, helpMessage: "Print help message")
 let allFlag = BoolOption(shortFlag: "A", longFlag: "all", required: false, helpMessage: "Clear archives")
 let dryRunFlag = BoolOption(shortFlag: "d", longFlag: "dry-run", required: false, helpMessage: "Dry run")
 let archivesFlag = BoolOption(shortFlag: "a", longFlag: "archives", required: false, helpMessage: "Clear archives")
 let backupDsymsFlag = StringOption(shortFlag: "b", longFlag: "backup-dsyms", helpMessage: "Backup dSYM files")
 
-commandLine.addOptions(allFlag, dryRunFlag, archivesFlag, backupDsymsFlag)
+commandLine.addOptions(helpFlag, allFlag, dryRunFlag, archivesFlag, backupDsymsFlag)
 
 do {
     try commandLine.parse()
 } catch {
     commandLine.printUsage(error)
     exit(EX_USAGE)
+}
+
+if helpFlag.value {
+    commandLine.printUsage()
+    exit(0)
 }
 
 print("All is \(allFlag.value)")
