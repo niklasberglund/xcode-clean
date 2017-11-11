d8740bd5aa9be14d3b756edd3e9ac66b5489cf7e
Niklas Berglund <niklas@asdasd.net>
2017-11-11 15:26:13 +0100

    Implemented diskSpace function

--- xcode-clean/Cleaner.swift
+++ xcode-clean/Cleaner.swift
@@ -17,7 +17,20 @@ enum DirectoryPath: String {
 
 class Cleaner {
     public func diskSpace(path: DirectoryPath) -> Int {
-        return 0
+        let expandedPath = Cleaner.expandTilde(path.rawValue)
+        var totalSize: Int = 0
+        
+        if let files = FileManager.default.subpaths(atPath: expandedPath) {
+            for filePath in files {
+                let filePathFull = "\(expandedPath)/\(filePath)"
+                
+                if let fileAttributes = try? FileManager.default.attributesOfItem(atPath: filePathFull) {
+                    totalSize += fileAttributes[FileAttributeKey.size] as! Int
+                }
+            }
+        }
+        
+        return totalSize
     }
     
     public func humanReadableDiskSpace(path: DirectoryPath) -> String {
@@ -26,5 +39,9 @@ class Cleaner {
     
     public func deleteContents(path: DirectoryPath) {
         
+    }
+    
+    private static func expandTilde(_ path: String) -> String {
+        return NSString(string: path).expandingTildeInPath
     }
 }
