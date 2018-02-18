a062abfb3dcfd83f0812e22cd27e4588570878df
Niklas Berglund <niklas@asdasd.net>
2018-02-18 16:18:10 +0100

    Integrated SSZipArchive

---
+++ xcode-clean/bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  bridging-header.h
+//  xcode-clean
+//
+//  Created by Niklas Berglund on 2018-02-18.
+//  Copyright © 2018 Niklas Berglund. All rights reserved.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+#import "SSZipArchive.h"
+
+#endif /* bridging_header_h */
